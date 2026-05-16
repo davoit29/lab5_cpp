46595309c0e55ee5026f5df0569d22761a3a8389
davoit29 <166952608+davoit29@users.noreply.github.com>
2026-05-16 11:03:55 +0300

    Update LoopCommand.h

--- src/LoopCommand.h
+++ src/LoopCommand.h
@@ -1,15 +1,15 @@
-#pragma once
+#pragma once   //это все команда на повторение
 #include "Command.h"
 #include <vector>
 
 class LoopCommand : public Command {
 private:
     int count;
-    std::vector<Command*> commands;
+    std::vector<Command*> commands; //адресса наследников
 
 public:
     LoopCommand(int c,
-                const std::vector<Command*>& cmds)
+                const std::vector<Command*>& cmds) //<Command*>& ссылка на вектор хранящий указатели Command  чтобы не копировать вектор
         : count(c), commands(cmds) {}
 
     void execute() override {
@@ -17,7 +17,7 @@ public:
         for (int i = 0; i < count; i++) {
 
             for (auto cmd : commands) {
-                cmd->execute();
+                cmd->execute(); //(*cmd).execute(); 
             }
         }
     }
